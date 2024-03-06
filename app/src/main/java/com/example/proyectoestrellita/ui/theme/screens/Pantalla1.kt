e7232c9a7b683c47f3f019799ddeb7b2a0ecfab2
The_Lonely_Otter <114146894+BraytonLorenzo@users.noreply.github.com>
2024-03-06 19:13:24 +0000

    Finalización
    Terminadas de poner todas las pantallas con todos sus elementos

--- app/src/main/java/com/example/proyectoestrellita/ui/theme/screens/Pantalla1.kt
+++ app/src/main/java/com/example/proyectoestrellita/ui/theme/screens/Pantalla1.kt
@@ -3,15 +3,19 @@ package com.example.proyectoestrellita.ui.theme.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import com.example.proyectoestrellita.R
 import com.example.proyectoestrellita.R.drawable.hampter
 
 @Composable
@@ -27,12 +31,19 @@ fun Pantalla1(navController: NavHostController) {
 
         Image(
             painter = painterResource(id = hampter ),
-            contentDescription = "sad hampter"
+            contentDescription = "sad hampter",
+            modifier = Modifier.size(300.dp),
+            contentScale = ContentScale.Fit
         )
-        Button(onClick = { /*TODO*/ }) {
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Yo al ver todas las tareas que me quedan pendientes")
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = { navController.navigate("Pantalla2") }) {
             Text(text = "Ir a pantalla 2")
         }
-
+        Spacer(modifier = Modifier.height(5.dp))
+        Button(onClick = { navController.navigate("Pantalla3") }) {
+            Text(text = "Ir a pantalla 3")
+        }
     }
-
 }
--- app/src/main/java/com/example/proyectoestrellita/ui/theme/screens/Pantalla2.kt
+++ app/src/main/java/com/example/proyectoestrellita/ui/theme/screens/Pantalla2.kt
@@ -1,13 +1,45 @@
 package com.example.proyectoestrellita.ui.theme.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.proyectoestrellita.R
 
 @Composable
 fun Pantalla2(navController: NavHostController) {
-    Button(onClick = { /*TODO*/ }) {
-        Text(text = "Ir a pantalla 2")
+
+    Column(
+
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ){
+
+        Image(
+            painter = painterResource(id = R.drawable.hormiga ),
+            contentDescription = "hormiga pobre",
+            modifier = Modifier.size(300.dp),
+            contentScale = ContentScale.Fit
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Yo en los momentos bajos pensando que no voy a poder con todo")
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(onClick = { navController.navigate("Pantalla1") }) {
+            Text(text = "Volver")
+        }
     }
 }
